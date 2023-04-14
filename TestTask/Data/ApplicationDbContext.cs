e3b33b4434619940f3b0b0ecdf678d9295c66ed2
Ivan Serdyukov <ivan.serdyukov270@gmail.com>
2023-04-14 23:08:43 +0300

    Implement logger

--- TestTask/Data/ApplicationDbContext.cs
+++ TestTask/Data/ApplicationDbContext.cs
@@ -1,16 +1,26 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using TestTask.Models;
 
 namespace TestTask.Data
 {
     public class ApplicationDbContext : IdentityDbContext<User>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        private readonly ILoggerFactory _loggerFactory;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, ILoggerFactory loggerFactory) : base(options)
         {
+            _loggerFactory = loggerFactory;
         }
 
         public override DbSet<User> Users { get; set; }
         public virtual DbSet<Team> Teams { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.EnableSensitiveDataLogging();
+            builder.UseLoggerFactory(_loggerFactory);
+        }
     }
 }
